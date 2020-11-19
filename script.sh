86e48128b9c95f66b3671b0b978d6177f47de102
Xavier de Liedekerke <xavier.deliedekerke@student.uclouvain.be>
2020-11-19 21:27:00 +0100

    essais

--- script.sh
+++ script.sh
@@ -1,48 +1,5 @@
 #!/bin/bash
 
-
-'rw' 'ph' 'pc'
-
-filename="measuresrw"
-
-echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./"$filename".csv
-
-for i in 2 4 8 16
-do
-
-    echo -n "$i" > ./"$filename".csv
-    for j in 1 2 3 4 5
-    do
-    
-        make clean
-        make rw
-        cTime=$(/usr/bin/time -f "%e" ./rw -R $i/2 -W $i/2 | tail -n 1) >&-
-        echo -n ",$cTime" > ./"$filename".csv
-    
-    done
-    echo -n "\n" > ./"$filename".csv
-done
-
-filename="measuresph"
-
-echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./"$filename".csv
-
-for i in 1 2 4 8 16
-do
-
-    echo -n "$i" > ./"$filename".csv
-    for j in 1 2 3 4 5
-    do
-    
-        make clean
-        make ph
-        cTime=$(/usr/bin/time -f "%e" ./ph -P $i | tail -n 1) >&-
-        echo -n ",$cTime" > ./"$filename".csv
-    
-    done
-    echo -n "\n" > ./"$filename".csv
-done
-
 filename="measurespc"
 
 echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./"$filename".csv
@@ -50,18 +7,18 @@ echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./"$filename".csv
 for i in 2 4 8 16
 do
 
-    echo -n "$i" > ./"$filename".csv
+    echo -n "$i" >> ./"$filename".csv
     for j in 1 2 3 4 5
     do
     
         make clean
         make pc
-        cTime=$(/usr/bin/time -f "%e" ./pc -P $i/2 -C $i/2 | tail -n 1) >&-
-        echo -n ",$cTime" > ./"$filename".csv
+        cTime=$(/usr/bin/time -f "%e" ./pc -P $i/2 -C $i/2 | tail -n 1) 
+        #echo "$cTime"
+        echo -n ",$cTime" >> ./"$filename".csv
     
     done
-    echo -n "\n" > ./"$filename".csv
+    echo -n "\n" >> ./"$filename".csv
 done
 
 
-SQUISH_NO_CAPTURE_OUTPUT=0
