d43ddcd6b5bf415d306963bfa4e06f564ba6baf4
Xavier de Liedekerke <xavier.deliedekerke@student.uclouvain.be>
2020-11-19 22:44:09 +0100

    script runnable

--- script.sh
+++ script.sh
@@ -1,24 +1,62 @@
 #!/bin/bash
+{
+    filename="measuresrw"
 
-filename="measurespc"
+    echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./Measures/"$filename".csv
 
-echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./"$filename".csv
-
-for i in 2 4 8 16
-do
-
-    echo -n "$i" >> ./"$filename".csv
-    for j in 1 2 3 4 5
+    for i in 2 4 8 16
     do
-    
-        make clean
-        make pc
-        cTime=$(/usr/bin/time -f "%e" ./pc -P $i/2 -C $i/2 | tail -n 1) 
-        #echo "$cTime"
-        echo -n ",$cTime" >> ./"$filename".csv
-    
+
+        echo -n "$i" >> ./Measures/"$filename".csv
+        for j in 1 2 3 4 5
+        do
+        
+            make clean
+            make rw
+            cTime=$(/usr/bin/time -f "%e" ./rw -R $i/2 -W $i/2 2>&1)
+            echo -n ",$cTime" >> ./Measures/"$filename".csv
+        
+        done
+        echo "" >> ./Measures/"$filename".csv
     done
-    echo -n "\n" >> ./"$filename".csv
-done
 
+    filename="measuresph"
 
+    echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./Measures/"$filename".csv
+
+    for i in 2 4 8 16
+    do
+
+        echo -n "$i" >> ./Measures/"$filename".csv
+        for j in 1 2 3 4 5
+        do
+        
+            make clean
+            make ph
+            cTime=$(/usr/bin/time -f "%e" ./ph -N $i 2>&1)
+            echo -n ",$cTime" >> ./Measures/"$filename".csv
+        
+        done
+        echo "" >> ./Measures/"$filename".csv
+    done
+
+    filename="measurespc"
+
+    echo "NB_THREADS,TIME_1,TIME_2,TIME_3,TIME_4,TIME_5" > ./Measures/"$filename".csv
+
+    for i in 2 4 8 16
+    do
+
+        echo -n "$i" >> ./Measures/"$filename".csv
+        for j in 1 2 3 4 5
+        do
+        
+            make clean
+            make pc
+            cTime=$(/usr/bin/time -f "%e" ./pc -P $i/2 -C $i/2 2>&1) 
+            echo -n ",$cTime" >> ./Measures/"$filename".csv
+        
+        done
+        echo "" >> ./Measures/"$filename".csv
+    done
+} &> /dev/null
