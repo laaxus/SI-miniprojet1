b85ea854decad1d4f09c7d00caaad2fb1d113c3a
Xavier de Liedekerke <xavier.deliedekerke@student.uclouvain.be>
2020-11-22 19:56:47 +0100

    ready for takeoff

--- graphing.py
+++ graphing.py
@@ -5,8 +5,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#def analyse_data (filename):
+#returns [[average], [lowerError], [higherError]] []
+def analyse_data (filename):
+	out = pd.read_csv('Measures/'+filename+'.csv', header = None, skiprows = 1).T
+	out.columns = out.iloc[0]
+	out.drop(0,inplace=True)
+	average = out.mean()
+	lowerError = average - out.min()
+	higherError = out.max() - average
+	return [average, lowerError, higherError]
 
-MeasuresPC = pd.read_csv('Measures/MeasuresPC.csv',header=None).T
+pc = analyse_data('MeasuresPC')
 
-print (MeasuresPC)
+fig1 = plt.figure()
+plt.errorbar(np.arange(1,9,1),pc[0], yerr = [pc[1],pc[2]], fmt = '-b', ecolor = 'r', capthick = 3, barsabove = True, label = 'Producer-Consumer')
+plt.ylim(0,4)
+plt.title('Producer-Consumer')
+plt.ylabel('Temps de compilation [S]')
+plt.xlabel('Nombre de threads')
+plt.legend(loc = 'upper right')
+plt.savefig("PC.pdf")
+plt.show()
+plt.close()
