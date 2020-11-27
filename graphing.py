0e74def24a87df1fa8d5dde24ca2154fa9996268
Xavier de Liedekerke <xavier.deliedekerke@student.uclouvain.be>
2020-11-27 11:32:25 +0100

    nouvelles mesures

--- graphing.py
+++ graphing.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#returns [[average], [lowerError], [higherError]] []
+#returns [[average], [standard deviation]]
 def analyse_data (filename):
 	out = pd.read_csv('Measures/'+filename+'.csv', header = None, skiprows = 1).T
 	out.columns = out.iloc[0]
 	out.drop(0,inplace=True)
 	average = out.mean()
-	lowerError = average - out.min()
-	higherError = out.max() - average
-	return [average, lowerError, higherError]
+	std = np.std(out)
+	return [average, std]
 
 pc = analyse_data('MeasuresPC')
 pc2 = analyse_data('MeasuresPC2')
@@ -24,9 +23,11 @@ ph2 = analyse_data('MeasuresPH2')
 ts = analyse_data('MeasuresTS')
 tts = analyse_data('MeasuresTTS')
 
+plt.style.use('ggplot')
+
 fig1 = plt.figure()
-plt.errorbar(np.arange(1,9,1),pc[0], yerr = [pc[1],pc[2]], fmt = '-b', ecolor = 'm', capthick = 3, barsabove = True, label = 'Stock Implementation')
-plt.errorbar(np.arange(1,9,1),pc2[0], yerr = [pc2[1],pc2[2]], fmt = '-g', ecolor = 'r', capthick = 3, barsabove = True, label = 'ASM Code')
+plt.errorbar(np.arange(1,9,1),pc[0], yerr = pc[1], capsize = 3, barsabove = True, label = 'Stock Implementation')
+plt.errorbar(np.arange(1,9,1),pc2[0], yerr = pc2[1], capsize = 3, barsabove = True, label = 'ASM Code')
 plt.ylim(0,4)
 plt.title('Producer-Consumer')
 plt.ylabel('Temps de compilation [S]')
@@ -37,8 +38,8 @@ plt.show()
 plt.close()
 
 fig2 = plt.figure()
-plt.errorbar(np.arange(1,9,1),rw[0], yerr = [rw[1],rw[2]], fmt = '-b', ecolor = 'm', capthick = 3, barsabove = True, label = 'Stock Implementation')
-plt.errorbar(np.arange(1,9,1),rw2[0], yerr = [rw2[1],rw2[2]], fmt = '-g', ecolor = 'r', capthick = 3, barsabove = True, label = 'ASM Code')
+plt.errorbar(np.arange(1,9,1),rw[0], yerr = rw[1], capsize = 3, barsabove = True, label = 'Stock Implementation')
+plt.errorbar(np.arange(1,9,1),rw2[0], yerr = rw2[1], capsize = 3, barsabove = True, label = 'ASM Code')
 plt.ylim(0,4)
 plt.title('Reader-Writer')
 plt.ylabel('Temps de compilation [S]')
@@ -49,8 +50,8 @@ plt.show()
 plt.close()
 
 fig3 = plt.figure()
-plt.errorbar(np.arange(1,9,1),ph[0], yerr = [ph[1],ph[2]], fmt = '-b', ecolor = 'm', capthick = 3, barsabove = True, label = 'Stock Implementation')
-plt.errorbar(np.arange(1,9,1),ph2[0], yerr = [ph2[1],ph2[2]], fmt = '-g', ecolor = 'r', capthick = 3, barsabove = True, label = 'ASM Code')
+plt.errorbar(np.arange(1,9,1),ph[0], yerr = ph[1], capsize = 3, barsabove = True, label = 'Stock Implementation')
+plt.errorbar(np.arange(1,9,1),ph2[0], yerr = ph2[1], capsize = 3, barsabove = True, label = 'ASM Code')
 plt.ylim(0,3)
 plt.title('Philosophe')
 plt.ylabel('Temps de compilation [S]')
@@ -62,8 +63,8 @@ plt.close()
 
 
 fig4 = plt.figure()
-plt.errorbar(np.arange(1,9,1),ts[0], yerr = [ts[1],ts[2]], fmt = '-b', ecolor = 'm', capthick = 3, barsabove = True, label = 'Test and Set')
-plt.errorbar(np.arange(1,9,1),tts[0], yerr = [tts[1],tts[2]], fmt = '-g', ecolor = 'r', capthick = 3, barsabove = True, label = 'Test and Test and Set')
+plt.errorbar(np.arange(1,9,1),ts[0], yerr = ts[1], capsize = 3, barsabove = True, label = 'Test and Set')
+plt.errorbar(np.arange(1,9,1),tts[0], yerr = tts[1], capsize = 3, barsabove = True, label = 'Test and Test and Set')
 plt.ylim(0,4.5)
 plt.title('Mutexes Analysis')
 plt.ylabel('Temps de compilation [S]')
